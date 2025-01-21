d9437c0790e55729c5e64d4049ea7ba44ec5d2c4
TNTSuperMan <tntsuperga@gmail.com>
2025-01-21 12:36:46 +0900

    プロキシ関数のテスト

--- test/func.test.ts
+++ test/func.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from "bun:test";
-import { watchFn } from "../src"
+import { watchFn, watchObj } from "../src"
 
 it("Call function", ()=>{
     const r: any[][] = [];
@@ -9,4 +9,26 @@ it("Call function", ()=>{
     expect(r[0][0]).toBe(3); //ret
     expect(r[0][1]).toBe(1); //arg1
     expect(r[0][2]).toBe(2); //arg2
+})
+
+it("Proxy function", ()=>{
+    const objlog: [PropertyKey, unknown, boolean][] = [];
+    const fnclog: [Function, string, any, ...any][] = [];
+    const fnobj = watchObj({
+        add(a: number, b: number){return a+b}
+    }, (...args)=>objlog.push(args), {
+        funcReporter: (...args) => fnclog.push(args)
+    })
+    fnobj.add(1,2);
+
+    expect(objlog.length).toBe(1);
+    expect(objlog[0][0]).toBe("add");
+    expect(objlog[0][2]).toBe(false);
+
+    expect(fnclog.length).toBe(1);
+    expect(fnclog[0].length).toBe(5);
+    expect(fnclog[0][1]).toBe("add");
+    expect(fnclog[0][2]).toBe(3);
+    expect(fnclog[0][3]).toBe(1);
+    expect(fnclog[0][4]).toBe(2);
 })
